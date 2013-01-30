b21a4faf2dae134c07489700d30684809d00f93e
ls <felipeal@google.com>
2013-01-30 12:19:27 -0800

    Added initial Makefile, still missing the mockito-target rule
    Change-Id: I5046c67313cada5529109ce54de6fda250f105a0

---
+++ Android.mk
@@ -0,0 +1,27 @@
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+LOCAL_PATH := $(call my-dir)
+
+# This is a "private" module - projects should use "mockito-target" instead!
+# It does not provide a MockMaker implementation, hence cannot be used directly.
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_STATIC_JAVA_LIBRARIES := junit4-target objenesis-target
+LOCAL_MODULE := mockito-api
+LOCAL_SDK_VERSION := 14
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_STATIC_JAVA_LIBRARY)
